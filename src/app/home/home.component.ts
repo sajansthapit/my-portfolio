e37559183266bf2638bdd624a430f9a521d4a384
Sajan Sthapit <sajansthapit99@gmail.com>
2024-04-16 08:54:35 +0545

    add: footer section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
 import { NavComponent } from '../nav/nav.component';
 import { AboutComponent } from '../sections/about/about.component';
-import { SkillsComponent } from '../sections/skills/skills.component';
 import { ExperienceComponent } from '../sections/experience/experience.component';
+import { SkillsComponent } from '../sections/skills/skills.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, NavComponent, AboutComponent, SkillsComponent, ExperienceComponent],
+  imports: [
+    CommonModule,
+    NavComponent,
+    AboutComponent,
+    SkillsComponent,
+    ExperienceComponent,
+    FooterComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
